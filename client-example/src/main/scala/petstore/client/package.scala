4f4772b29919f149eea92b9cf7755cbfacc5bc9a
Scala Steward <me@scala-steward.org>
2020-02-21 08:11:00 +0100

    Update scalatest to 3.1.1

--- client-example/src/main/scala/petstore/client/package.scala
+++ client-example/src/main/scala/petstore/client/package.scala
@@ -24,14 +24,10 @@ import org.http4s.QueryParamEncoder
 package object client {
 
   def localDateTimeQueryParamEncoder(formatter: DateTimeFormatter): QueryParamEncoder[LocalDateTime] =
-    QueryParamEncoder[String].contramap[LocalDateTime] { i: LocalDateTime =>
-      formatter.format(i)
-    }
+    QueryParamEncoder[String].contramap[LocalDateTime] { i: LocalDateTime => formatter.format(i) }
 
   def localDateQueryParamEncoder(formatter: DateTimeFormatter): QueryParamEncoder[LocalDate] =
-    QueryParamEncoder[String].contramap[LocalDate] { i: LocalDate =>
-      formatter.format(i)
-    }
+    QueryParamEncoder[String].contramap[LocalDate] { i: LocalDate => formatter.format(i) }
 
   implicit val isoLocalDateTimeEncoder: QueryParamEncoder[LocalDateTime] =
     localDateTimeQueryParamEncoder(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
